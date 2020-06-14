b4f9989c7997086a64464fdb16b6dfdd63a42515
Reshab Vaigankar <reshab.vaigankar@conaug.com>
2020-06-14 16:48:45 +0530

    zfd

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express()
 // db.once('open', () => console.log('Connected to Database'))
 
 // app.use(cors())
-app.use(express.json())
+// app.use(express.json())
 
 
 app.use('/subscribers', subscribersRouter)
