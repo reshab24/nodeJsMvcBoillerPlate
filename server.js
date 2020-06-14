418ae1709ee9f24e250156e2d66e079894df4123
Reshab Vaigankar <reshab.vaigankar@conaug.com>
2020-06-14 11:22:19 +0530

    herokuTest

--- server.js
+++ server.js
@@ -2,23 +2,27 @@ require('dotenv').config()
 const subscribersRouter = require('./apis/subscriber/router')
 const usersRouter = require('./apis/users/router')
 const authRouter=require('./apis/auth/auth.router') 
-const cors = require('cors')
+// const cors = require('cors')
 
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
+// const express = require('express')
+// const app = express()
+// const mongoose = require('mongoose')
 
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
-const db = mongoose.connection
-db.on('error', (error) => console.error(error))
-db.once('open', () => console.log('Connected to Database'))
+// mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+// const db = mongoose.connection
+// db.on('error', (error) => console.error(error))
+// db.once('open', () => console.log('Connected to Database'))
 
-app.use(cors())
-app.use(express.json())
+// app.use(cors())
+// app.use(express.json())
 
 
-app.use('/subscribers', subscribersRouter)
-app.use('/users', usersRouter)
-app.use('/auth',authRouter)
+// app.use('/subscribers', subscribersRouter)
+// app.use('/users', usersRouter)
+// app.use('/auth',authRouter)
+app.get('/',(req, res)=> {
+    res.send('Tks heroku');
+})
 
+const PORT = process.env.PORT|| 3000
 app.listen(3000, () => console.log('Server Started'))
