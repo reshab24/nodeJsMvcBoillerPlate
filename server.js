52ccaeb7364ac375c86f00c8e21fb060d9c7e605
Reshab Vaigankar <reshab.vaigankar@conaug.com>
2020-06-14 16:39:52 +0530

    jsdghf

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
-require('dotenv').config()
-const subscribersRouter = require('./apis/subscriber/router')
-const usersRouter = require('./apis/users/router')
-const authRouter=require('./apis/auth/auth.router') 
-const cors = require('cors')
+// require('dotenv').config()
+// const subscribersRouter = require('./apis/subscriber/router')
+// const usersRouter = require('./apis/users/router')
+// const authRouter=require('./apis/auth/auth.router') 
+// const cors = require('cors')
 const express = require('express')
 const app = express()
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true })
-const db = mongoose.connection
-db.on('error', (error) => console.error(error))
-db.once('open', () => console.log('Connected to Database'))
+// mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true })
+// const db = mongoose.connection
+// db.on('error', (error) => console.error(error))
+// db.once('open', () => console.log('Connected to Database'))
 
-app.use(cors())
-app.use(express.json())
+// app.use(cors())
+// app.use(express.json())
 
 
-app.use('/subscribers', subscribersRouter)
-app.use('/users', usersRouter)
-app.use('/auth',authRouter)
-// app.get('/', function(req, res){
-//     res.send('Tks heroku');
-// })
+// app.use('/subscribers', subscribersRouter)
+// app.use('/users', usersRouter)
+// app.use('/auth',authRouter)
+app.get('/', function(req, res){
+    res.send('Tks heroku');
+})
 app.listen(process.env.PORT || 5000, () => console.log('Server Started'))
