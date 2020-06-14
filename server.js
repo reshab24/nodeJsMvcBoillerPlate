f334c9be1923d831f58a53ced119798fa6c71d0a
Reshab Vaigankar <reshab.vaigankar@conaug.com>
2020-06-14 16:55:32 +0530

    s

--- server.js
+++ server.js
@@ -5,15 +5,15 @@ const authRouter=require('./apis/auth/auth.router')
 // const cors = require('cors')
 const express = require('express')
 const app = express()
-// const mongoose = require('mongoose')
+const mongoose = require('mongoose')
 
-// mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true })
-// const db = mongoose.connection
-// db.on('error', (error) => console.error(error))
-// db.once('open', () => console.log('Connected to Database'))
+mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('Connected to Database'))
 
-// app.use(cors())
-// app.use(express.json())
+app.use(cors())
+app.use(express.json())
 
 
 app.use('/subscribers', subscribersRouter)
