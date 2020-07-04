03d76bcafc5ffca646f15c8832f03675bbc6aa24
Reshab Vaigankar <reshab.vaigankar@conaug.com>
2020-07-04 16:36:52 +0530

    zdsasd

--- server.js
+++ server.js
@@ -2,10 +2,7 @@ require('dotenv').config()
 const mongoose = require('mongoose')
 const app=require('./app');
 
-const DB = process.env.DATABASE.replace(
-    '<PASSWORD>',
-    process.env.DATABASE_PASSWORD
-  );
+const DB =  process.env.MONGODB_URI || 'mongodb://localhost/GECD';
   
   mongoose
     .connect(DB, {
